410f55c9d4c4c3d3f23e404afa02d1847198a0a2
MSI\GA2A <h102319001@hiratagakuen.onmicrosoft.com>
2024-10-16 16:36:40 +0900

    mapを一度にすべてを表示、弾数制御のみ

---
+++ Assets/sugimoto/Pistol.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    public int bullet_num = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bullet_num = Random.Range(8, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
