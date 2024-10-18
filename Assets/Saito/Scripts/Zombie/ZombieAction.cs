300e68a258e08d8cb4e94a131903dec248f197e5
DESKTOP-M30FNO0\GA2A <h1202319029@hiratagakuen.onmicrosoft.com>
2024-10-18 15:32:35 +0900

    ゾンビほぼ完成

--- Assets/Saito/Scripts/Zombie/ZombieAction.cs
+++ Assets/Saito/Scripts/Zombie/ZombieAction.cs
@@ -11,6 +11,9 @@ public class ZombieAction : ZombieBase
     [SerializeField]//のけぞりの力
     private float knockBackPower = 10.0f;
 
+    [SerializeField]//死亡時に生成するオブジェクト
+    private GameObject dropItemPrefab;
+
     Rigidbody rb;
 
     public override void SetUpZombie()
@@ -21,12 +24,12 @@ public class ZombieAction : ZombieBase
     
     /// <summary>
     /// のけぞり
-    /// 後で引数にのけぞり方向を指定したい
     /// </summary>
-    public void KnockBack()
+    public void KnockBack(Vector3 _vec)
     {
+        Vector3.Normalize(_vec);//正規化
         //のけぞり
-        rb.AddForce(transform.forward * -1.0f * knockBackPower, ForceMode.Impulse);
+        rb.AddForce(_vec * knockBackPower, ForceMode.Impulse);
     }
     /// <summary>
     /// 死亡処理
@@ -34,6 +37,28 @@ public class ZombieAction : ZombieBase
     public void Dead()
     {
         Debug.Log("ゾンビが死亡");
-        Destroy(gameObject);
+
+        ItemDrop();//アイテムドロップ
+
+        Destroy(gameObject);//とりあえず即destroy
+    }
+
+    /// <summary>
+    /// オブジェクトをドロップする
+    /// </summary>
+    private void ItemDrop()
+    {
+        //ドロップするアイテムの指定が無ければreturn
+        if (dropItemPrefab == null) return;
+
+        //とりあえず同じ位置にドロップ
+        Vector3 dropPos = transform.position;
+
+        //生成
+        Instantiate(
+            dropItemPrefab, 
+            dropPos, 
+            Quaternion.identity
+            );
     }
 }
--- Assets/Saito/Scripts/Zombie/ZombieAnimation.cs
+++ Assets/Saito/Scripts/Zombie/ZombieAnimation.cs
@@ -2,33 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ゾンビのアニメーションを行うクラス
+/// </summary>
 public class ZombieAnimation : ZombieBase
 {
     [SerializeField]//アニメーター
     private Animator animator;
 
+    //移動アニメーションの種類
+    enum MoveType
+    { 
+        IDLE,
+        WALK,
+        RUN
+    }
+
+    //現在の移動アニメーション
+    MoveType correntMoveType;
+
+
+    /// <summary>
+    /// 初期設定
+    /// </summary>
     public override void SetUpZombie()
     {
-        
+        correntMoveType = MoveType.WALK;
     }
 
     public void Attack()
     {
+        Debug.Log("animation:Attack");
         animator.SetTrigger("Attack");
     }
 
     public void Walk()
     {
+        //同じアニメーションを複数呼び出ししないように
+        if (correntMoveType == MoveType.WALK) return;
+        correntMoveType = MoveType.WALK;
+
+        Debug.Log("animation:Walk");
         animator.SetTrigger("Walk");
     }
 
     public void Idle()
     {
+        //同じアニメーションを複数呼び出ししないように
+        if (correntMoveType == MoveType.IDLE) return;
+        correntMoveType = MoveType.IDLE;
+
+        Debug.Log("animation:Idle");
         animator.SetTrigger("Idle");
     }
 
     public void Run()
     {
+        //同じアニメーションを複数呼び出ししないように
+        if (correntMoveType == MoveType.RUN) return;
+        correntMoveType = MoveType.RUN;
+
+        Debug.Log("animation:Run");
         animator.SetTrigger("Run");
     }
 }
