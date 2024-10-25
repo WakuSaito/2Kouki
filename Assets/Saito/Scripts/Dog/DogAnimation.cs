ecd69450cc751675b3046ee0c31cf876073b659d
DESKTOP-3GUQ93R\GA2A <h1202319029@hiratagakuen.onmicrosoft.com>
2024-10-25 15:52:30 +0900

    犬が指定されたオブジェクトへ突進し攻撃するように 離れるとプレイヤーへ移動する

--- Assets/Saito/Scripts/Dog/DogAnimation.cs
+++ Assets/Saito/Scripts/Dog/DogAnimation.cs
@@ -2,67 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// ゾンビのアニメーションを行うクラス
-/// </summary>
-public class ZombieAnimation : ZombieBase
+public class DogAnimation : DogBase
 {
     [SerializeField]//アニメーター
     private Animator animator;
-
     //移動アニメーションの種類
+    //Managerの方で管理してもいいかも
     enum MoveType
-    { 
+    {
         IDLE,
         WALK,
         RUN
     }
 
     //現在の移動アニメーション
-    MoveType correntMoveType;
+    MoveType currentMoveType;
 
 
     /// <summary>
     /// 初期設定
     /// </summary>
-    public override void SetUpZombie()
+    public override void SetUpDog()
     {
-        correntMoveType = MoveType.WALK;
+        currentMoveType = MoveType.IDLE;
     }
 
     public void Attack()
     {
-        Debug.Log("zombie:Attack");
+        Debug.Log("dog:Attack");
         animator.SetTrigger("Attack");
     }
 
     public void Walk()
     {
         //同じアニメーションを複数呼び出ししないように
-        if (correntMoveType == MoveType.WALK) return;
-        correntMoveType = MoveType.WALK;
+        if (currentMoveType == MoveType.WALK) return;
+        currentMoveType = MoveType.WALK;
 
-        Debug.Log("zombie:Walk");
+        Debug.Log("dog:Walk");
         animator.SetTrigger("Walk");
     }
 
     public void Idle()
     {
         //同じアニメーションを複数呼び出ししないように
-        if (correntMoveType == MoveType.IDLE) return;
-        correntMoveType = MoveType.IDLE;
+        if (currentMoveType == MoveType.IDLE) return;
+        currentMoveType = MoveType.IDLE;
 
-        Debug.Log("zombie:Idle");
+        Debug.Log("dog:Idle");
         animator.SetTrigger("Idle");
     }
 
     public void Run()
     {
         //同じアニメーションを複数呼び出ししないように
-        if (correntMoveType == MoveType.RUN) return;
-        correntMoveType = MoveType.RUN;
+        if (currentMoveType == MoveType.RUN) return;
+        currentMoveType = MoveType.RUN;
 
-        Debug.Log("zombie:Run");
+        Debug.Log("dog:Run");
         animator.SetTrigger("Run");
     }
+
 }
--- Assets/Saito/Scripts/Dog/DogManager.cs
+++ Assets/Saito/Scripts/Dog/DogManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 /*
 後ほどクラスを分割したい
@@ -9,53 +12,132 @@ using UnityEngine;
 ・その他アクションごとに分割
 */
 
+//自動でアタッチされるスクリプト
+[RequireComponent(typeof(DogMove))]
+[RequireComponent(typeof(DogAnimation))]
+
 /// <summary>
 /// 犬のマネージャークラス
 /// </summary>
 public class DogManager : MonoBehaviour
 {
+    /// <summary>
+    /// 操作するクラス
+    /// </summary>
+    [SerializeField]
+    private DogBase[] dogBases;
+
+    private DogMove dogMove;
+    private DogAnimation dogAnimation;
+
+    [SerializeField]//噛みついている時間
+    private double biteStaySec = 4.0;
+
+    [SerializeField]//待機状態になるプレイヤーとの距離
+    private float stayPlayerDistance = 5.0f;
+
+
     //攻撃対象オブジェクト
     private GameObject attackTargetObj;
+    //プレイヤー
+    private GameObject playerObj;
 
     //攻撃対象に突進中
     private bool isChargeTarget = false;
     //行動停止
     private bool isStopAction = false;
 
+    private void Awake()
+    {
+        //プレイヤー取得
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var dog in dogBases)
+        {
+            //各クラスでオーバーライドした初期設定実行
+            dog.SetUpDog();
+
+            //下記クラスに該当するか確認し代入
+            //どうやら違うクラスでTryGetComponentするとnullが代入されるっぽいのでnullチェック
+            if (dogMove == null) dog.TryGetComponent(out dogMove);
+            if (dogAnimation == null) dog.TryGetComponent(out dogAnimation);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isStopAction) {
+            dogMove.StopMove();
+            return; 
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))//デバッグ用
+        {
+            GameObject obj = GameObject.FindGameObjectWithTag("Zombie");
+            OrderAttack(obj);
+        }
+
         if (isChargeTarget)//突進
         {
             //攻撃対象が存在しないならreturn
             //攻撃対象が途中でDestroyされた場合の挙動注意
-            if (attackTargetObj == null)
-            {
+            if (attackTargetObj == null){
                 isChargeTarget = false;//突進中断
                 return;
             }
 
             //attackTargetObjの位置に向かって移動する
+            dogMove.LookAtObject(attackTargetObj);
+            dogMove.RunFront();//移動
+
+            dogAnimation.Run();//アニメーション
 
             //攻撃対象に限りなく近づいたら
-            //噛みつく
+            if (GetObjectDistance(attackTargetObj) < 0.5f)
+            {
+                //噛みつく
+                BiteZombie(attackTargetObj);
+            }
         }
         else//通常時の移動
         {
-            //if(プレイヤーとの距離が離れている)
-            //プレイヤーの方に移動する
-            //障害物の判定に注意
 
+            //if(プレイヤーとの距離が離れている)
+            if (GetObjectDistance(playerObj) > stayPlayerDistance)
+            {
+                //プレイヤーの方に移動する
+                //障害物の判定に注意
+                dogMove.LookAtObject(playerObj);
+                dogMove.RunFront();
+
+                dogAnimation.Run();//アニメーション
+
+            }
+            else
+            {
+                dogAnimation.Idle();
+                dogMove.StopMove();
+            }
             //else if(近くなら)
             //とりあえずは何もしない
             //プレイヤーの近くでウロウロさせたい　範囲内のランダム位置を目標として移動みたいな
         }
+    }
+
+    /// <summary>
+    /// 対象のオブジェクトとの距離を求める
+    /// </summary>
+    private float GetObjectDistance(GameObject _target)
+    {
+        Vector3 pos = transform.position;
+        Vector3 targetPos = _target.transform.position;
+        pos.y = 0; targetPos.y = 0;//y方向を計算しない
+        return Vector3.Distance(targetPos, pos);
     }
 
     /// <summary>
@@ -73,8 +155,31 @@ public class DogManager : MonoBehaviour
     /// </summary>
     private void BiteZombie(GameObject _zombieObj)
     {
+        ZombieManager zombieManager;
         //attackTargetObjからZombieManagerを取得し、FreezePositionを呼び出し
+        attackTargetObj.TryGetComponent(out zombieManager);
+        if (zombieManager == null) return;
 
+        zombieManager.FreezePosition(biteStaySec);//ゾンビを停止
+
+        dogAnimation.Attack();
         isStopAction = true;
+        isChargeTarget = false;
+
+        DelayRunAsync(
+            biteStaySec,
+            () => {
+                isStopAction = false; 
+            });
+    }
+
+    /// <summary>
+    /// 遅らせてActionを実行するasync
+    /// </summary>
+    private async ValueTask DelayRunAsync(double wait_sec, Action action)
+    {
+        // ディレイ処理
+        await Task.Delay(TimeSpan.FromSeconds(wait_sec));
+        action();
     }
 }
