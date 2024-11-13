40e342bd311f37f08ff42fbe0c42121c13f64d72
DESKTOP-3GUQ93R\GA2A <h1202319001@hiratagakuen.onmicrosoft.com>
2024-11-13 11:49:24 +0900

    インベントリ完成

--- Assets/sugimoto/Inventory.cs
+++ Assets/sugimoto/Inventory.cs
@@ -19,6 +19,9 @@ public class Inventory : ID
     [SerializeField] Transform frame_pos;
     [SerializeField] GameObject bullet_text_obj;
     [SerializeField] Text bullet_text;
+    float color = 0.0f;
+    float display_timer = 0.0f;
+    bool display_flag = true;
 
     public enum WEAPON_ID
     {
@@ -67,6 +70,7 @@ public class Inventory : ID
                 Screen.lockCursor = false;
                 item_inventory_flag = true;
                 item_inventory.SetActive(true);
+
             }
             else
             {
@@ -74,6 +78,13 @@ public class Inventory : ID
                 item_inventory_flag = false;
                 item_inventory.SetActive(false);
             }
+        }
+
+        if(item_inventory_flag)
+        {
+            //武器インベントリ表示
+            display_flag = true;
+            display_timer = 0.0f;
         }
     }
 
@@ -242,6 +253,10 @@ public class Inventory : ID
                         //プレイヤーの現在の武器をピストルに変更
                         GetComponent<player>().hand_weapon = weapon_hand_obj[(int)WEAPON_ID.PISTOL];
                     }
+                    else
+                    {
+                        _item.SetActive(false);
+                    }
                 }
                 else
                 {
@@ -271,10 +286,13 @@ public class Inventory : ID
                 //現在の武器非表示
                 weapon_hand_obj[weapon_cnt].SetActive(false);
                 weapon_sprite_obj[weapon_cnt].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+                //インベントリ表示
+                display_timer = 0.0f;
+                display_flag = true;
             }
 
-            //マウスホイール上回し
-            if (mouse_wheel > 0)
+            //マウスホイール下回し
+            if (mouse_wheel < 0)
             {
                 //次の武器インベントリへ
                 weapon_cnt++;
@@ -303,8 +321,8 @@ public class Inventory : ID
                     }
                 }
             }
-            //下回し
-            if (mouse_wheel < 0)
+            //上回し
+            if (mouse_wheel > 0)
             {
                 //前の武器インベントリ
                 weapon_cnt--;
@@ -345,6 +363,57 @@ public class Inventory : ID
 
         }
 
+        //武器インベントリ表示非表示
+        {
+            display_timer += Time.deltaTime;
+
+            if (display_timer >= 5.0f)
+            {
+                display_flag = false;
+                color = 0.0f;
+            }
+
+            if (!display_flag)
+            {
+                color += Time.deltaTime;
+
+                for (int i = 0; i < weapon_sprite_obj.Length; i++)
+                {
+                    //親オブジェ
+                    weapon_sprite_obj[i].transform.parent.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, weapon_sprite_obj[i].transform.parent.GetComponent<Image>().color.a - color);
+                    //オブジェ本体
+                    weapon_sprite_obj[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, weapon_sprite_obj[i].GetComponent<Image>().color.a - color);
+                    //枠オブジェ
+                    frame_pos.GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f, frame_pos.GetComponent<Image>().color.a - color);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < weapon_sprite_obj.Length; i++)
+                {
+                    //親オブジェ
+                    weapon_sprite_obj[i].transform.parent.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+
+                    //オブジェ本体
+                    if (hand_weapon == (WEAPON_ID)i)
+                    {
+                        weapon_sprite_obj[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                    }
+                    else if (weapon_hand_obj[i] != null)
+                    {
+                        weapon_sprite_obj[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+                    }
+                    else
+                    {
+                        weapon_sprite_obj[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+                    }
+
+                    //枠オブジェ
+                    frame_pos.GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }
+            }
+        }
+
         //配置、UI設定
         bullet_text_obj.SetActive(false);
         switch (hand_weapon)
@@ -371,6 +440,9 @@ public class Inventory : ID
         weapon_hand_obj[weapon_cnt].SetActive(true);
         weapon_sprite_obj[weapon_cnt].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         frame_pos.position = weapon_sprite_obj[weapon_cnt].transform.position;
+        //武器インベントリ表示
+        display_flag = true;
+        display_timer = 0.0f;
     }
 
     void ParentChildren(GameObject _parent, GameObject _child)
