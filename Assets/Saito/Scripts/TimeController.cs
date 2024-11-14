28cca5ad6c3b39f280ce7f85b3ff6da32dbfa43a
DESKTOP-3GUQ93R\GA2A <h1202319029@hiratagakuen.onmicrosoft.com>
2024-11-14 12:29:53 +0900

    時間経過表示用にHourMinuteクラス作成

--- Assets/Saito/Scripts/TimeController.cs
+++ Assets/Saito/Scripts/TimeController.cs
@@ -2,6 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class HourMinute
+{
+    private const int MAX_HOUR = 24;
+    private const int MAX_MINUTE = 60;
+
+    private int hour = 0;
+    private int minute = 0;
+
+    public HourMinute(int _hour, int _minute)
+    {
+        AddHour(_hour);
+        AddMinute(_minute);
+    }
+
+    public int GetHour() { return hour; }
+    public int GetMinute() { return minute; }
+
+    public int AddMinute(int _minute)
+    {
+        int sumMinute = minute + _minute;
+
+        if (sumMinute >= MAX_MINUTE)
+        {
+            minute = sumMinute % MAX_MINUTE;
+            AddHour(sumMinute / MAX_MINUTE);
+        }
+        else if (sumMinute < 0)
+        {
+            minute = sumMinute + MAX_MINUTE * ((sumMinute * -1) / MAX_MINUTE + 1);
+            AddHour(sumMinute / MAX_MINUTE -1);
+        }
+        else
+            minute = sumMinute;
+
+        return minute;
+    }
+    public int AddHour(int _hour)
+    {
+        int sumHour = hour + _hour;
+
+        if (sumHour >= MAX_HOUR)
+        {
+            hour = sumHour % MAX_HOUR;
+        }
+        else if (sumHour < 0)
+        {
+            hour = sumHour + MAX_HOUR * ((sumHour * -1) / MAX_HOUR + 1);
+        }
+        else
+            hour = sumHour;
+
+        return hour;
+    }
+
+    public string GetTimeString()
+    {
+        string timeString = "";
+
+        if (hour < 10)
+            timeString += "0";
+        timeString += hour.ToString() + ":";
+
+        if (minute < 10)
+            timeString += "0";
+        timeString += minute.ToString();
+
+        return timeString;
+    }
+}
+
 //時間経過スクリプト　（昼夜の処理など）
 public class TimeController : MonoBehaviour
 {
