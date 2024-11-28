935736cd78b45900c8793ea061c73bca1b699211
DESKTOP-LM5JQ0T\Setun <h1202319001@hiratagakuen.onmicrosoft.com>
2024-11-28 23:40:01 +0900

    住宅地作成、多分復帰できた

---
+++ Assets/sugimoto_2/1_Script/Item/DestoryRigitbody.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryRigitbody : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (rigidbody != null) 
+        {
+            Destroy(rigidbody);
+            GetComponent<DestoryRigitbody>().enabled = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+}
