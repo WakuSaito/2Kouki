27f32749b8f271687b440b307e003fb46f63e331
DESKTOP-LM5JQ0T\Setun <h1202319001@hiratagakuen.onmicrosoft.com>
2024-12-04 11:14:30 +0900

    script勉強

--- Assets/sugimoto_2/1_Script/player/Inventory/ItemInformation.cs
+++ Assets/sugimoto_2/1_Script/player/Inventory/ItemInformation.cs
@@ -20,15 +20,27 @@ public class ItemInformation : MonoBehaviour
         DRINK_1,
         DRINK_2,
         EMERGENCY_PACK,
-        PISTOL,
         BULLET,
+        PISTOL,
+        ASSAULT,
+        SHOTGUN,
+        HAND,
+        KNIFE,
+        DOG_DIRECTION,
     }
 
-    public struct ItemInfo
+    public ITEM_TYPE type;
+    public ITEM_ID id;
+    public int get_num;
+    public int stack_max;
+
+    //コンストラクタ
+    public ItemInformation(ITEM_TYPE _type, ITEM_ID _id, int _get_num, int _stack_max)
     {
-        public ITEM_TYPE type;
-        public ITEM_ID id;
-        public int get_num;
-        public int stack_max;
+        //自分のクラスのメンバーに引数の値を代入する
+        type = _type;
+        id = _id;
+        get_num = _get_num;
+        stack_max = _stack_max;
     }
 }
---
+++ Assets/sugimoto_2/1_Script/player/Inventory/ItemSetting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSetting : MonoBehaviour
+{
+    //ItemInformation iteminfo;
+    ItemInventory ItemInventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemInventory = new ItemInventory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
