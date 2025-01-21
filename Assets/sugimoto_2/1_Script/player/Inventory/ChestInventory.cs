5dc728ab6bd0d6ed895209b214e1fd3bd4b6b5a9
DESKTOP-3GUQ93R\GA2A <h1202319001@hiratagakuen.onmicrosoft.com>
2025-01-21 12:34:06 +0900

    アイテムインベントリ使用できないアイテムマーク追加

--- Assets/sugimoto_2/1_Script/player/Inventory/ChestInventory.cs
+++ Assets/sugimoto_2/1_Script/player/Inventory/ChestInventory.cs
@@ -11,8 +11,6 @@ using UnityEngine.UI;
 public class ChestInventory : MonoBehaviour
 {
     public InventoryClass m_inventory;
-    inventoryManager m_inventoryManager;
-    player m_Player;
 
     public int m_sloatSize = 10;
     public Transform[] m_spriteTrans;
@@ -25,20 +23,13 @@ public class ChestInventory : MonoBehaviour
     [SerializeField] GameObject m_inventoryManagerObj;
     public GameObject m_ChestUIObj;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// スタート関数
+    /// インベントリクラス作成
+    /// </summary>
     void Start()
     {
+        //インベントリクラス作成
         m_inventory = new InventoryClass(m_sloatSize, m_slotBoxTrans);
-        m_inventoryManager = m_inventoryManagerObj.GetComponent<inventoryManager>();
-        m_Player = m_PlayerObj.GetComponent<player>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (m_inventoryManager.inventory_state == INVENTORY.CHEST)
-        {
-            m_inventory.SetUI(m_spriteTrans, m_Text);
-        }
     }
 }
