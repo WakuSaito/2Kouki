2189096df1c153e991fa39da638e1bf33fec5cf2
DESKTOP-3GUQ93R\GA2A <h1202319001@hiratagakuen.onmicrosoft.com>
2025-01-29 16:32:33 +0900

    バグ修正

--- Assets/sugimoto_2/1_Script/player/IK.cs
+++ Assets/sugimoto_2/1_Script/player/IK.cs
@@ -37,11 +37,27 @@ public class IK : MonoBehaviour
             case SLOT_ORDER.KNIFE:
                 onIK = true;
                 break;
+            case SLOT_ORDER.DOG:
+                handR = player.GetComponent<player>().hand_weapon.GetComponent<SetHandIK>().HandR;
+                onIK = true;
+                break;
             default:
                 onIK = false;
                 break;
         }
 
+        //if (player.GetComponent<player>().hand_weapon.GetComponent<SetHandIK>() == null) return;
+
+        //if(player.GetComponent<player>().hand_weapon.GetComponent<SetHandIK>().HandL!=null)
+        //{
+        //    handL = player.GetComponent<player>().hand_weapon.GetComponent<SetHandIK>().HandL;
+        //    onIK = true;
+        //}
+        //if(player.GetComponent<player>().hand_weapon.GetComponent<SetHandIK>().HandR != null)
+        //{
+        //    handR = player.GetComponent<player>().hand_weapon.GetComponent<SetHandIK>().HandR;
+        //    onIK = true;
+        //}
 
         if (!onIK) return;
 
@@ -73,10 +89,15 @@ public class IK : MonoBehaviour
                 animator.SetIKRotation(AvatarIKGoal.RightHand, knife_hand_R.rotation);
             }
         }
-    }
-
-    void OnIK()
-    {
-        onIK = true;
+        else if (player.GetComponent<InventoryWeapon>().m_selectSlot == SLOT_ORDER.DOG)
+        {
+            if (knife_hand_R != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKPosition(AvatarIKGoal.RightHand, knife_hand_R.position);
+                animator.SetIKRotation(AvatarIKGoal.RightHand, knife_hand_R.rotation);
+            }
+        }
     }
 }
